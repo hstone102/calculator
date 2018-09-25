986803e5120a29c2b0df583bd84d3a4c78be4559
Holly Stone <hstone102@gmail.com>
2018-09-25 16:59:50 -0400

    initial changes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// This is the JavaScript file we edit
+var printOut = function printOut() {
+  console.log(document.getElementById('/'));
+}; //if (onclick = number)
+
+
+var getInput = function getInput() {
+  var zero = document.getElementById('0');
+  console.log(zero); //store that number in a string
+
+  var numberString = document.getElementById('numberfromhtml');
+  ; //if next click is number... keep in this loop, add it to the string
+  //make the string a number
+  //else
+  //GetOperator = () => {
+  //read in the GetOperator
+  //loop back to the top to get more numbers
+  //}
+};
+
+calculate = function calculate() {};
+
+clear = function clear() {};
