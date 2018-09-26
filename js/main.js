81acd42dc3899fec1ee5c61d475874cbb162031d
Holly Stone <hstone102@gmail.com>
2018-09-26 09:50:44 -0400

    More changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
 "use strict";
 
 // This is the JavaScript file we edit
-var printOut = function printOut() {
-  console.log(document.getElementById('/'));
-}; //if (onclick = number)
-
-
+//if (onclick = number)
 var getInput = function getInput() {
-  var zero = document.getElementById('0');
-  console.log(zero); //store that number in a string
-
-  var numberString = document.getElementById('numberfromhtml');
-  ; //if next click is number... keep in this loop, add it to the string
-  //make the string a number
-  //else
-  //GetOperator = () => {
-  //read in the GetOperator
-  //loop back to the top to get more numbers
-  //}
-};
-
-calculate = function calculate() {};
-
-clear = function clear() {};
+  //send the number to the top to display
+  var one = document.getElementById('1');
+  var integer1 = parseInt(one, 10);
+  var two = parseInt(document.getElementById('2').value, 10);
+  var three = parseInt(document.getElementById('3').value, 10);
+  var four = parseInt(document.getElementById('4').value, 10);
+  var five = parseInt(document.getElementById("5").value, 10);
+  var six = parseInt(document.getElementById("6").value, 10);
+  var seven = parseInt(document.getElementById("7").value, 10);
+  var eight = parseInt(document.getElementById("8").value, 10);
+  var nine = parseInt(document.getElementById("9").value, 10);
+  var zero = parseInt(document.getElementById("0").value, 10);
+  var multiply = document.getElementById('X');
+  var decimal = document.getElementById('.');
+  var fraction = document.getElementById('/');
+  var add = document.getElementById('+');
+  var subtract = document.getElementById('-');
+  console.log("integer1");
+  console.log("two");
+  console.log("three");
+  console.log("four" + "three");
+}; //store that number in a string
+//let numberString = document.getElementById('numberfromhtml');;
+//if next click is number... keep in this loop, add it to the string
+//make the string a number
+//else
+//GetOperator = () => {
+//read in the GetOperator
+//loop back to the top to get more numbers
+//}
+//}
+//calculate = () => {
+//}
+//clear = () =>{
+//}
