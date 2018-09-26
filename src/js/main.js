8038a8c0f81f02cb05c0f482bef3d73acc10e4c7
Holly Stone <hstone102@gmail.com>
2018-09-26 09:50:44 -0400

    More changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,37 @@
 // This is the JavaScript file we edit
-const printOut = () => {
-  console.log(document.getElementById('/'));
-}
+
 
 //if (onclick = number)
+
 const getInput = () => {
+  //send the number to the top to display
+  var one = document.getElementById('1');
+  var integer1 = parseInt(one, 10);
+  var two = parseInt(document.getElementById('2').value, 10);
+  var three = parseInt(document.getElementById('3').value, 10);
+  var four = parseInt(document.getElementById('4').value, 10);
+  var five = parseInt(document.getElementById("5").value, 10);
+  var six = parseInt(document.getElementById("6").value, 10);
+  var seven = parseInt(document.getElementById("7").value, 10);
+  var eight = parseInt(document.getElementById("8").value, 10);
+  var nine = parseInt(document.getElementById("9").value, 10);
+  var zero = parseInt(document.getElementById("0").value, 10);
+  var multiply = document.getElementById('X');
+  var decimal = document.getElementById('.');
+  var fraction = document.getElementById('/');
+  var add = document.getElementById('+');
+  var subtract = document.getElementById('-');
 
-var zero = document.getElementById('0');
+  console.log("integer1");
+  console.log("two");
+  console.log("three");
+  console.log("four" + "three");
 
-console.log(zero);
+  }
+
+
 //store that number in a string
-let numberString = document.getElementById('numberfromhtml');;
+//let numberString = document.getElementById('numberfromhtml');;
 
 
 //if next click is number... keep in this loop, add it to the string
@@ -26,16 +47,16 @@ let numberString = document.getElementById('numberfromhtml');;
   //}
 
 
-}
+//}
 
 
-calculate = () => {
+//calculate = () => {
 
 
 
 
-}
+//}
 
-clear = () =>{
+//clear = () =>{
 
-}
+//}
